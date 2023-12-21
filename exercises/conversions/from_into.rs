cb711676b3b2971e787203bacc45fee18f9ec239
cowile <cowile@pm.me>
2023-12-21 12:42:28 -0600

    WIP: rustlings: conversions: Conversion exercises
    How to parse strings into types.

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+	let mut parts = s.split(',');
+	let name = parts.next();
+	let age = parts.next().map(|a| a.parse::<usize>());
+
+	if let (Some(n), Some(Ok(a))) = (name, age) {
+	    if n.len() > 0 {
+		return Person { name: n.to_string(), age: a, }
+	    }
+	}
+	
+	return Person::default();
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,30 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+	if s.len() == 0 {
+	    return Err(ParsePersonError::Empty);
+	}
+	
+	let mut parts = s.split(',');
+
+	let name = parts.next();
+	let age = parts.next();
+
+	if name.is_none() || age.is_none() || parts.count() != 0 {
+	    return Err(ParsePersonError::BadLen);
+	}
+
+	let name = name.unwrap();
+	let age = age.unwrap();
+	
+	if name.len() == 0 {
+	    return Err(ParsePersonError::NoName);
+	}
+
+	match age.parse::<usize>() {
+	    Ok(a) => Ok(Person { name: name.to_string(), age: a, }),
+	    Err(a) => Err(ParsePersonError::ParseInt(a)),
+	}
     }
 }
 
